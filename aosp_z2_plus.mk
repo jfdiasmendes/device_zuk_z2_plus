c9a6c24d86ef63a04fc6802676638676c250f024
davide <davidevinavil@gmail.com>
2017-11-08 10:20:39 +0100

    switch to jdc

--- aosp_z2_plus.mk
+++ aosp_z2_plus.mk
@@ -23,8 +23,8 @@ $(call inherit-product, device/zuk/z2_plus/device.mk)
 # Boot animation resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Inherit some common AOSP-OMS stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_z2_plus
 PRODUCT_DEVICE := z2_plus
